d72e0818dd4488f0b734fc37acf20d80064fc449
Boy Kurniawan <boykurniawan123@gmail.com>
2021-05-29 19:12:38 +0700

    up

--- application/views/satpam/load.php
+++ application/views/satpam/load.php
@@ -4,18 +4,25 @@
 	<meta charset="utf-8">
 	<title></title>
 	<base href="<?php echo base_url() ?>">
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" /> 
+	<link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" /> 
 	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> -->
 </head>
 <body>
 <div style="margin-top: 10px;">
-	<ul data-role="listview" data-inset="true">
+	<!-- <ul data-role="listview" data-inset="true">
 		<li><a href="#" id="jadwal_satpam">Buat Jadwal Satpam</a></li>
 		<li><a href="#" id="data_satpam">Data Satpam</a></li>
 		<li><a href="#" id="jadwal_satpam_now">Jadwal Satpam Hari ini</a></li>
 		<li><a href="#" id="lokasi_semua_satpam">Lihat Semua Posisi Satpam (Sedang Jaga)</a></li>
-	</ul>
+	</ul> -->
+
+	<a href="#" class="btn btn-block btn-success" id="jadwal_satpam">Buat Jadwal Satpam</a>
+	<a href="#" class="btn btn-block btn-success" id="data_satpam">Data Satpam</a>
+	<a href="#" class="btn btn-block btn-success" id="jadwal_satpam_now">Jadwal Satpam Hari ini</a>
+	<a href="#" class="btn btn-block btn-success" id="lokasi_semua_satpam">Lihat Semua Posisi Satpam (Sedang Jaga)</a>
+
 </div>
 
 <script type="text/javascript">
