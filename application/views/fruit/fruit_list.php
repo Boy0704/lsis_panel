a6250f7f4fb161f6568f9a7c7f42f28c36a46d07
Boy Kurniawan <boykurniawan123@gmail.com>
2021-05-31 19:45:29 +0700

    up

--- application/views/fruit/fruit_list.php
+++ application/views/fruit/fruit_list.php
@@ -87,10 +87,13 @@
                     }
                 }
                 if (isset($_GET['id_level'])) {
-                    if ( $fruit->status != 'final' && ($this->input->get('id_level') != '7' OR $this->input->get('id_level') != '14') ) {
+                    if ($fruit->status != 'final') {
+                        if ( $this->input->get('id_level') != '7' OR $this->input->get('id_level') != '14' ) {
                         echo ' | '; 
                         echo anchor(site_url('fruit/delete/'.$fruit->id_fruit).'?'.param_get(),'<span class="label label-danger">Hapus</span>','onclick="javasciprt: return confirm(\'Are You Sure ?\')"');
                     }
+                    }
+                    
                 }
 				 
 				?>
